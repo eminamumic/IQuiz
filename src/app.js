8192b7bf6e077f2004dedd43ce3cdb50689aea39
eminamumic <emimumic1@gmail.com>
2025-03-13 20:11:55 +0100

    Move getData() function from app.js to dashboard.js

--- src/app.js
+++ src/app.js
@@ -4,15 +4,3 @@ const landingPageBtn = document.querySelector('#landingpage-btn')
 landingPageBtn.addEventListener('click', () => {
   window.location.href = 'dashboard.html'
 })
-
-async function getData() {
-  try {
-    const response = await fetch('https://the-trivia-api.com/v2/questions')
-    const data = await response.json()
-    console.log(data)
-  } catch (error) {
-    console.error('Greška pri dohvaćanju podataka:', error)
-  }
-}
-
-getData()
