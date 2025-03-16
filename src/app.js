b280e9d506dd12688f7ec4dae9238dd33f9cb99f
eminamumic <emimumic1@gmail.com>
2025-03-16 20:18:25 +0100

    Adding new categoryes on langing page and new images

--- src/app.js
+++ src/app.js
@@ -1,3 +1,60 @@
-document.querySelector('#landingpage-btn').addEventListener('click', () => {
-  window.location.href = 'dashboard.html'
+import { getData } from './data.js'
+
+const userSelection = {}
+
+document.querySelector('#science-card').addEventListener('click', function () {
+  userSelection.category = this.getAttribute('data-category')
 })
+
+document.querySelector('#music-card').addEventListener('click', function () {
+  userSelection.category = this.getAttribute('data-category')
+})
+
+document.querySelector('#sport-card').addEventListener('click', function () {
+  userSelection.category = this.getAttribute('data-category')
+})
+
+document.querySelector('#easy').addEventListener('click', function () {
+  userSelection.difficulty = this.getAttribute('data-difficulty')
+})
+
+document.querySelector('#medium').addEventListener('click', function () {
+  userSelection.difficulty = this.getAttribute('data-difficulty')
+})
+
+document.querySelector('#hard').addEventListener('click', function () {
+  userSelection.difficulty = this.getAttribute('data-difficulty')
+})
+
+document
+  .querySelector('#question-number')
+  .addEventListener('input', function () {
+    userSelection.limit = parseInt(this.value) || 1
+  })
+
+document.querySelector('#name-input').addEventListener('input', function () {
+  userSelection.userName = this.value
+})
+
+document
+  .querySelector('#start-quiz-btn')
+  .addEventListener('click', async () => {
+    if (!userSelection.category || !userSelection.difficulty) {
+      alert('Molimo odaberite kategoriju i težinu prije početka kviza!')
+      return
+    }
+
+    await getData(
+      userSelection.category,
+      userSelection.difficulty,
+      userSelection.limit
+    )
+
+    window.location.href = 'quiz.html'
+  })
+
+document
+  .querySelector('#back-to-landingpage-btn')
+  .addEventListener('click', () => {
+    window.location.href = 'index.html'
+  })
