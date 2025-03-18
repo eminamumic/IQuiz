2ccf496513ca74dcc3f10b80885eb2a71f4b52c1
eminamumic <emimumic1@gmail.com>
2025-03-18 22:00:25 +0100

    Created style.js for styling functions and moved them from quiz.js to style.js  functions: hiddenIcons, resetQuestionIcons, highlightCorrectAnswer, highlightIncorrectAnswer

---
+++ src/style.js
@@ -0,0 +1,46 @@
+function hiddenIcons() {
+  document.querySelector('.decor9').classList.add('hidden')
+  document.querySelector('.decor10').classList.add('hidden')
+  document.querySelector('.decor11').classList.add('hidden')
+}
+
+function resetQuestionIcons() {
+  document.querySelector('.decor9').classList.remove('hidden')
+  document.querySelector('.decor10').classList.add('hidden')
+  document.querySelector('.decor11').classList.add('hidden')
+}
+
+function highlightCorrectAnswer(correctAnswer) {
+  document.querySelector('.decor9').classList.add('hidden')
+  document.querySelector('.decor11').classList.remove('hidden')
+
+  const buttons = document.querySelectorAll('button')
+  buttons.forEach((button) => {
+    if (button.textContent === correctAnswer) {
+      button.classList.add('correct')
+    } else {
+      button.classList.add('incorrect')
+    }
+  })
+}
+
+function highlightIncorrectAnswer(correctAnswer) {
+  document.querySelector('.decor9').classList.add('hidden')
+  document.querySelector('.decor10').classList.remove('hidden')
+
+  const buttons = document.querySelectorAll('button')
+  buttons.forEach((button) => {
+    if (button.textContent === correctAnswer) {
+      button.classList.add('correct')
+    } else {
+      button.classList.add('incorrect')
+    }
+  })
+}
+
+export {
+  hiddenIcons,
+  resetQuestionIcons,
+  highlightCorrectAnswer,
+  highlightIncorrectAnswer,
+}
