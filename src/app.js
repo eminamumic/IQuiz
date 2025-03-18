fda8b0b316a11d3ffcd564b310c691579ee5b7a4
eminamumic <emimumic1@gmail.com>
2025-03-18 21:50:44 +0100

    Added forEach method to attach event listeners to cards in app.js

--- src/app.js
+++ src/app.js
@@ -2,58 +2,18 @@ import { getData } from './data.js'
 
 const userSelection = {}
 
-document.querySelector('#science-card').addEventListener('click', function () {
-  userSelection.category = this.getAttribute('data-category')
-})
-
-document.querySelector('#music-card').addEventListener('click', function () {
-  userSelection.category = this.getAttribute('data-category')
-})
-
-document.querySelector('#sport-card').addEventListener('click', function () {
-  userSelection.category = this.getAttribute('data-category')
-})
-
-document.querySelector('#film-card').addEventListener('click', function () {
-  userSelection.category = this.getAttribute('data-category')
-})
-
-document.querySelector('#history-card').addEventListener('click', function () {
-  userSelection.category = this.getAttribute('data-category')
-})
-
-document.querySelector('#society-card').addEventListener('click', function () {
-  userSelection.category = this.getAttribute('data-category')
-})
-
-document
-  .querySelector('#geography-card')
-  .addEventListener('click', function () {
+const cardCategories = document.querySelectorAll('[data-category]')
+cardCategories.forEach((card) => {
+  card.addEventListener('click', function () {
     userSelection.category = this.getAttribute('data-category')
   })
-
-document.querySelector('#food-card').addEventListener('click', function () {
-  userSelection.category = this.getAttribute('data-category')
 })
 
-document.querySelector('#general-card').addEventListener('click', function () {
-  userSelection.category = this.getAttribute('data-category')
-})
-
-document.querySelector('#art-card').addEventListener('click', function () {
-  userSelection.category = this.getAttribute('data-category')
-})
-
-document.querySelector('#easy').addEventListener('click', function () {
-  userSelection.difficulty = this.getAttribute('data-difficulty')
-})
-
-document.querySelector('#medium').addEventListener('click', function () {
-  userSelection.difficulty = this.getAttribute('data-difficulty')
-})
-
-document.querySelector('#hard').addEventListener('click', function () {
-  userSelection.difficulty = this.getAttribute('data-difficulty')
+const cardDifficulties = document.querySelectorAll('[data-difficulty]')
+cardDifficulties.forEach((card) => {
+  card.addEventListener('click', function () {
+    userSelection.difficulty = this.getAttribute('data-difficulty')
+  })
 })
 
 document
