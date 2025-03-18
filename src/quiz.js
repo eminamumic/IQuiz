d2df0b50428c46131535b4532f9d53052d96d91a
eminamumic <emimumic1@gmail.com>
2025-03-18 23:23:38 +0100

    Change the names of the global variables.

--- src/quiz.js
+++ src/quiz.js
@@ -4,17 +4,14 @@ import {
   highlightCorrectAnswer,
   highlightIncorrectAnswer,
   goBackBtn,
-  displayNoDataMessage,
+  disableAnswerButtons,
 } from './style.js'
 
-let countdown = null
-let questionData = null
-let currentQuestionIndex = 0
-let score = {
-  correct: 0,
-  incorrect: 0,
-}
-const quizContainer = document.querySelector('#container')
+let COUNTDOWN = null
+let QUESTION_DATA = null
+let CURRENT_QUESTION_INDEX = 0
+let SCORE = 0
+const QUIZ_CONTAINER = document.querySelector('#container')
 
 document.addEventListener('DOMContentLoaded', () => {
   startQuiz()
@@ -29,31 +26,52 @@ function startQuiz() {
     return
   }
 
-  questionData = JSON.parse(storedQuestionData)
+  QUESTION_DATA = JSON.parse(storedQuestionData)
   showQuestion()
 }
 
-function mixAnswers(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1))
-    ;[array[i], array[j]] = [array[j], array[i]]
+function displayNoDataMessage(resultBtn) {
+  QUIZ_CONTAINER.innerHTML =
+    '<h1 class="header-text">No data available for the quiz.</h1>'
+  QUIZ_CONTAINER.appendChild(resultBtn)
+  resultBtn.addEventListener('click', () => {
+    window.location.href = 'index.html'
+  })
+}
+
+function showQuestion() {
+  QUIZ_CONTAINER.innerHTML = ''
+
+  if (CURRENT_QUESTION_INDEX >= QUESTION_DATA.length) {
+    showResults()
+    localStorage.clear()
+    return
   }
+
+  questionBody()
+  resetQuestionIcons()
+}
+
+function questionBody() {
+  const questionObj = QUESTION_DATA[CURRENT_QUESTION_INDEX]
+  displayQuestionText(questionObj)
+  displayAnswerButtons(questionObj)
+  startTimer(questionObj)
 }
 
 function checkAnswer(answer, correctAnswer) {
-  clearTimeout(countdown)
+  clearTimeout(COUNTDOWN)
   disableAnswerButtons()
 
   if (answer === correctAnswer) {
     highlightCorrectAnswer(answer)
-    score.correct++
+    SCORE++
   } else {
     highlightIncorrectAnswer(correctAnswer)
-    score.incorrect++
   }
 
   setTimeout(() => {
-    currentQuestionIndex++
+    CURRENT_QUESTION_INDEX++
     showQuestion()
   }, 2000)
 }
@@ -62,7 +80,7 @@ function displayQuestionText(questionObj) {
   const questionText = document.createElement('h1')
   questionText.classList.add('header-text-question')
   questionText.textContent = questionObj.question.text
-  quizContainer.appendChild(questionText)
+  QUIZ_CONTAINER.appendChild(questionText)
 }
 
 function displayAnswerButtons(questionObj) {
@@ -76,7 +94,7 @@ function displayAnswerButtons(questionObj) {
     const answerButton = createAnswerButton(answer, questionObj.correctAnswer)
     answersContainer.appendChild(answerButton)
   })
-  quizContainer.appendChild(answersContainer)
+  QUIZ_CONTAINER.appendChild(answersContainer)
 }
 
 function createAnswerButton(answer, correctAnswer) {
@@ -89,64 +107,50 @@ function createAnswerButton(answer, correctAnswer) {
   return answerButton
 }
 
+function mixAnswers(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
+
 function startTimer(questionObj) {
   let timerDiv = document.createElement('div')
-  timerDiv.id = 'timer'
-  timerDiv.textContent = '15'
-  timerDiv.classList.add('timer')
-  quizContainer.appendChild(timerDiv)
-
   let timer = 15
-  countdown = setInterval(() => {
+  timerDiv.id = 'timer'
+  timerDiv.textContent = `${timer}`
+  QUIZ_CONTAINER.appendChild(timerDiv)
+
+  COUNTDOWN = setInterval(() => {
     document.querySelector('#timer').textContent = timer
     timer--
 
     if (timer < 0) {
-      clearInterval(countdown)
+      clearInterval(COUNTDOWN)
       highlightIncorrectAnswer(questionObj.correctAnswer)
+      CURRENT_QUESTION_INDEX++
       setTimeout(() => {
-        currentQuestionIndex++
         showQuestion()
       }, 2000)
     }
   }, 1000)
 }
 
-function questionBody() {
-  const questionObj = questionData[currentQuestionIndex]
-  displayQuestionText(questionObj)
-  displayAnswerButtons(questionObj)
-  startTimer(questionObj)
-}
-
-function showQuestion() {
-  quizContainer.innerHTML = ''
-
-  if (currentQuestionIndex >= questionData.length) {
-    showResults()
-    localStorage.clear()
-    return
-  }
-
-  questionBody()
-  resetQuestionIcons()
-}
-
 function showResults() {
-  quizContainer.innerHTML = ''
+  QUIZ_CONTAINER.innerHTML = ''
 
   let resultText = ''
   let resultImage = ''
 
-  if (score.correct === questionData.length) {
+  if (SCORE === QUESTION_DATA.length) {
     resultText = `Excellent! You achieved a perfect score!`
     resultImage = '/materijal/4.png'
     hiddenIcons()
-  } else if (score.correct >= questionData.length * 0.75) {
+  } else if (SCORE >= QUESTION_DATA.length * 0.75) {
     resultText = 'Great! You achieved a high score!'
     resultImage = '/materijal/5.png'
     hiddenIcons()
-  } else if (score.correct >= questionData.length * 0.5) {
+  } else if (SCORE >= QUESTION_DATA.length * 0.5) {
     resultText = 'Good! But you can do better.'
     resultImage = '/materijal/op.png'
     hiddenIcons()
@@ -163,16 +167,16 @@ function displayResultMessage(resultText, resultImage) {
   const resultTextElement = document.createElement('h1')
   resultTextElement.textContent = resultText
   resultTextElement.classList.add('header-text-end')
-  quizContainer.appendChild(resultTextElement)
+  QUIZ_CONTAINER.appendChild(resultTextElement)
 
   const resultImageElement = document.createElement('img')
   resultImageElement.classList.add('end-image')
   resultImageElement.src = resultImage
   resultImageElement.alt = 'Quiz result'
-  quizContainer.appendChild(resultImageElement)
+  QUIZ_CONTAINER.appendChild(resultImageElement)
 
   const resultBtn = goBackBtn('Go back')
-  quizContainer.appendChild(resultBtn)
+  QUIZ_CONTAINER.appendChild(resultBtn)
   resultBtn.addEventListener('click', () => {
     window.location.href = 'index.html'
   })
